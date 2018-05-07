bb1e22a3ae804fb85373b8617e49a06f7948e5db
pzl <dan@panzarel.la>
2018-05-06 21:51:15 -0400

    override plugin settings with project-level settings

--- helpers/settings.py
+++ helpers/settings.py
@@ -2,33 +2,19 @@ import sublime
 
 def get_settings(window=False):
 
-	settings = sublime.load_settings("MonkeyCBuild.sublime-settings")
-	#@todo: override ^ with project settings
+	plugin_settings = sublime.load_settings("MonkeyCBuild.sublime-settings")
+
+	settings = {
+		"sdk": plugin_settings.get("sdk",""),
+		"key": plugin_settings.get("key","")
+	}
 
 	window_vars = {}
 	if window:
-		window_vars = window.extract_variables()
-		""" Looks like:
-		{
-			'project': '/home/dan/dev/sub-projects/monkeyc.sublime-project',
-			'project_extension': 'sublime-project',
-			'file': '/home/dan/dev/Sublime-MonkeyC/monkey_build.py',
-			'file_path': '/home/dan/dev/Sublime-MonkeyC',
-			'platform': 'Linux',
-			'folder': '/home/dan/dev/Sublime-MonkeyC',
-			'packages': '/home/dan/.config/sublime-text-3/Packages',
-			'file_name': 'monkey_build.py',
-			'file_base_name': 'monkey_build',
-			'project_name': 'monkeyc.sublime-project',
-			'file_extension': 'py',
-			'project_base_name': 'monkeyc',
-			'project_path': '/home/dan/dev/sub-projects'
-		}
-		"""
+		window_vars = window.extract_variables() # get project path, folder, etc
 
-		project_data = window.project_data() # JSON from .sublime-project
-		project_settings = (project_data or {}).get("monkeyc",{}) # should override setting with these
-
-		view_settings = window.active_view().settings() # ???
+		# config from .sublime-project. Check for "monkeyc" key.
+		project_settings = (window.project_data() or {}).get("monkeyc",{})
+		settings.update(project_settings) # override plugin settings with project-specific
 
 	return settings,window_vars
